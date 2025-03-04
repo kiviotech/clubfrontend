da861aeae4341fd298b116747a594f777a05cbb1
prithviraj m pillai <prithvi_mp@yahoo.in>
2025-03-04 13:07:19 +0530

    testing

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { SplashScreen, Stack } from "expo-router";
 import { useFonts } from "expo-font";
+import ErrorBoundary from '../components/ErrorBoundary';
 
 
 SplashScreen.preventAutoHideAsync();
@@ -31,13 +32,14 @@ const RootLayout = () => {
   if (!fontsLoaded && !error) return null;
 
   return (
-    
+    <ErrorBoundary>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{headerShown: false}} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="pages" options={{ headerShown: false }} />
       </Stack>
+    </ErrorBoundary>
   );
 };
 
